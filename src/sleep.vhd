a6e0292d0255629235d112941c13a7494b2f1e5c
David RACODON <david.racodon@gmail.com>
2026-04-07 14:12:54 +0200

    My new feature...

--- src/sleep.vhd
+++ src/sleep.vhd
@@ -8,11 +8,12 @@ entity sleep is
     i1 : in std_logic;
     i2 : in std_logic;
     o1 : out std_logic;
-    o2 : out std_logic
+    o2 : out std_logic;
+    toto : out std_logic_vector(0 to 3)
   );
 end entity;
 
-architecture rtl of sleep is
+architecture MyRTL of sleep is
 begin
   p1 : process (clk) is
   begin
@@ -32,7 +33,10 @@ begin
         o2 <= '0';
       else
         o2 <= i1 or i2;
+        toto(0) <= '1';
+        toto(1) <= i1 or i2;
       end if;
     end if;
   end process;
+  toto(3) <= i1 or i2;
 end architecture;
